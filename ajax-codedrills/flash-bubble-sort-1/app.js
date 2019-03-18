7982ab6810ca03ec7f25e10ef4cd57e5679f91ec
lalitha <lalithapelluri6@gmail.com>
2019-03-17 22:12:59 -0700

    ajax code drills

---
+++ ajax-codedrills/flash-bubble-sort-1/app.js
@@ -0,0 +1,24 @@
+// build a function that uses bubble sort to sort an array from
+// highest value to lowest value and returns the new sorted array
+
+function bubbleSort(Arr) {
+// ================= code goes here ===========================
+    let len = Arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (Arr[j] > Arr[j + 1]) {
+                let tmp = Arr[j];
+                Arr[j] = Arr[j + 1];
+                Arr[j + 1] = tmp;
+            }
+        }
+    }
+    return Arr;
+
+
+// ============================================================
+}
+
+var testArray = [34, 23, 3, 76, 20, 84, 18, 9];
+testArray = bubbleSort(testArray);
+console.log(testArray);

c2632cadf13bb824d761868ba8169342a1c4963b
lalitha <lalithapelluri6@gmail.com>
2019-03-26 19:52:15 -0700

    ajax code drills

--- nodejs-codedrills/quotes/app.js
+++ nodejs-codedrills/quotes/app.js
@@ -1,20 +1,29 @@
 var fs = require("fs");
-var args = process.argv;
+var args = process.argv[2];
 
 var arr = [];
 var Quotes;
 var Person = '';
 var msg = "That person never said anything";
+var message;
 
 for(var i=0; i<args.length; i++){
     arr.push(args[i]);
 }
+console.log(arr);
 
-fs.readFile("quotes.txt", "utf8", function(err,data){
-    Quotes = data.split('');
-    for(var j=0; j<Quotes.length; j++){
-        var result = Quotes[i];
-        message = result[0];
+fs.readFile("./quotes.txt", "utf8", function(err,data){
+    Quotes = data.split(';');
+    console.log(Quotes);
+    for(var j=0; j<Quotes.length; j++) {
+        var result = Quotes[j].split('-');
+        console.log(result);
+        if (result === null) {
+            console.log(msg);
+        } else if(result[1]=== args){
+            message = result[0];
+        }
+
     }
     console.log(message);
 });

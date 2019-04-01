43637d757e49b4c89c96a24c29ca040102c665f9
lalitha <lalithapelluri6@gmail.com>
2019-03-31 17:06:41 -0700

    node js code drills

---
+++ nodejs-codedrills/fsquotes/prompt.js
@@ -0,0 +1,22 @@
+
+var fs = require("fs");
+var args = process.argv[2];
+
+var arr = [];
+for(var i=0; i<args.length; i++){
+    arr.push(args[i]);
+}
+var Quote = arr[arr.length - 1];
+args.pop();
+var person = arr.join();
+console.log(arr);
+
+var finalquote = [];
+Quote = '"' + Quote + '"';
+finalquote.push("-");
+finalquote.push(person);
+
+fs.appendFile("./quotes.txt", "utf8", function(err,data){
+    Quotes = data.split(';');
+    console.log("Quotes added");
+});
